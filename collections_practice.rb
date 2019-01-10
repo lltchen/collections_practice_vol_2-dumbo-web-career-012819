1a00039ede7021ca4ef298fc956c2190dadc24a6
lltchen <lltchen1@gmail.com>
2019-01-10 20:05:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,5 +1,5 @@
-def begins_with_r("Collections")
-  Collections.each do |object|
+def begins_with_r(collections)
+  collections.each do |object|
     if object.start_with?("r")
       return true
     else
