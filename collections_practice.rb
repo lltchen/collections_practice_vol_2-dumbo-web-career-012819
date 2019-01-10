46d1c56b2915c70073f0bb952f733d277c614d41
lltchen <lltchen1@gmail.com>
2019-01-10 20:03:47 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,10 @@
-# your code goes here
+def begins_with_r(Collections)
+  Collections.each do |object|
+    if object.start_with?("r")
+      return true
+    else 
+      return false
+    end
+end
+
+    
